50cf826df97484b99c422c9eaf6f24dde6cea632
tallerdenaturalezacfavo-web <tallerdenaturalezacfavo@gmail.com>
2025-11-03 19:45:35 +0100

    Add files via upload

--- Sesión de trabajo.js
+++ Sesión de trabajo.js
@@ -4,7 +4,7 @@ var p; // shortcut to reference prototypes
 var lib={};var ss={};var img={};
 lib.ssMetadata = [
 		{name:"Sesión de trabajo_atlas_1", frames: [[0,790,911,1200],[0,0,1400,788]]},
-		{name:"Sesión de trabajo_atlas_2", frames: [[1551,1575,465,106],[0,1643,720,66],[0,1575,1549,66],[1202,0,436,431],[0,687,1589,442],[0,1131,1589,442],[0,0,1200,685],[1640,395,218,394],[1875,0,161,184],[1640,0,233,393],[1875,186,150,150]]}
+		{name:"Sesión de trabajo_atlas_2", frames: [[1551,1575,465,106],[0,1643,720,66],[0,1575,1549,66],[1202,0,436,431],[0,687,1589,442],[0,1131,1589,442],[0,0,1200,685],[1640,395,218,394],[1875,0,161,184],[1640,0,233,393],[1875,186,88,88]]}
 ];
 
 
@@ -28,48 +28,48 @@ lib.ssMetadata = [
 
 
 
-(lib.CachedBmp_21 = function() {
+(lib.CachedBmp_7 = function() {
 	this.initialize(ss["Sesión de trabajo_atlas_2"]);
 	this.gotoAndStop(0);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.CachedBmp_20 = function() {
+(lib.CachedBmp_6 = function() {
 	this.initialize(ss["Sesión de trabajo_atlas_2"]);
 	this.gotoAndStop(1);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.CachedBmp_19 = function() {
+(lib.CachedBmp_5 = function() {
 	this.initialize(ss["Sesión de trabajo_atlas_2"]);
 	this.gotoAndStop(2);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.CachedBmp_18 = function() {
-	this.initialize(img.CachedBmp_18);
+(lib.CachedBmp_4 = function() {
+	this.initialize(img.CachedBmp_4);
 }).prototype = p = new cjs.Bitmap();
 p.nominalBounds = new cjs.Rectangle(0,0,2569,110);
 
 
-(lib.CachedBmp_17 = function() {
+(lib.CachedBmp_3 = function() {
 	this.initialize(ss["Sesión de trabajo_atlas_2"]);
 	this.gotoAndStop(3);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.CachedBmp_16 = function() {
+(lib.CachedBmp_2 = function() {
 	this.initialize(ss["Sesión de trabajo_atlas_2"]);
 	this.gotoAndStop(4);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.CachedBmp_15 = function() {
+(lib.CachedBmp_1 = function() {
 	this.initialize(ss["Sesión de trabajo_atlas_2"]);
 	this.gotoAndStop(5);
 }).prototype = p = new cjs.Sprite();
@@ -122,7 +122,23 @@ p.nominalBounds = new cjs.Rectangle(0,0,2569,110);
 	this.initialize(ss["Sesión de trabajo_atlas_2"]);
 	this.gotoAndStop(10);
 }).prototype = p = new cjs.Sprite();
+// helper functions:
 
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop, this.reversed));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
 
 
 (lib.Símbolo13 = function(mode,startPosition,loop,reversed) {
@@ -149,6 +165,28 @@ if (reversed == null) { reversed = false; }
 p.nominalBounds = new cjs.Rectangle(0,0,1283.6,54);
 
 
+(lib.Símbolo7 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Capa_1
+	this.instance = new lib.Mapadebits8();
+	this.instance.setTransform(-44,-44);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.Símbolo7, new cjs.Rectangle(-44,-44,88,88), null);
+
+
 (lib.Símbolo5 = function(mode,startPosition,loop,reversed) {
 if (loop == null) { loop = true; }
 if (reversed == null) { reversed = false; }
@@ -161,7 +199,7 @@ if (reversed == null) { reversed = false; }
 	cjs.MovieClip.apply(this,[props]);
 
 	// Capa_1
-	this.instance = new lib.CachedBmp_21();
+	this.instance = new lib.CachedBmp_7();
 	this.instance.setTransform(0,0,0.5,0.5);
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
@@ -285,29 +323,6 @@ if (reversed == null) { reversed = false; }
 p.nominalBounds = new cjs.Rectangle(-4.1,-9.9,8.2,19.9);
 
 
-(lib.HOME = function(mode,startPosition,loop,reversed) {
-if (loop == null) { loop = true; }
-if (reversed == null) { reversed = false; }
-	var props = new Object();
-	props.mode = mode;
-	props.startPosition = startPosition;
-	props.labels = {};
-	props.loop = loop;
-	props.reversed = reversed;
-	cjs.MovieClip.apply(this,[props]);
-
-	// Capa_1
-	this.instance = new lib.Mapadebits8();
-	this.instance.setTransform(0,0,0.3467,0.3467);
-
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
-
-	this._renderFirstFrame();
-
-}).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(0,0,52,52);
-
-
 (lib.Ealbedo = function(mode,startPosition,loop,reversed) {
 if (loop == null) { loop = true; }
 if (reversed == null) { reversed = false; }
@@ -337,16 +352,16 @@ if (reversed == null) { reversed = false; }
 
 	this.GrafEalbedo = new lib.Interpolación1("synched",0);
 	this.GrafEalbedo.name = "GrafEalbedo";
-	this.GrafEalbedo.setTransform(201.25,541.95,60.4702,33.5391,0,0,0,0.3,0);
+	this.GrafEalbedo.setTransform(-256.7,23.35,60.4702,33.5391,0,0,0,0.3,0);
 	this.GrafEalbedo._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1).to({_off:false},0).to({_off:true,regX:0.3,scaleX:60.4702,scaleY:33.5391,x:201.25},5).wait(2));
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1).to({_off:false},0).to({_off:true,regX:0.3,scaleX:60.4702,scaleY:33.5391,x:-256.7,y:23.35},5).wait(2));
 	this.timeline.addTween(cjs.Tween.get(this.GrafEalbedo).wait(1).to({_off:false},5).to({x:266.1,y:7614.2},1).wait(1));
 
 	this._renderFirstFrame();
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(-64.8,0,559.9,7949.2);
+p.nominalBounds = new cjs.Rectangle(-522.7,-310.3,1017.8000000000001,8259.5);
 
 
 // stage content:
@@ -386,11 +401,46 @@ if (reversed == null) { reversed = false; }
 		*/
 		
 		this.stop();
+		
+		
+		/* Hacer clic para ir al fotograma y detener
+		Al hacer clic en la instancia del símbolo especificado, la cabeza lectora se mueve hasta el fotograma especificado en la línea de tiempo y detiene la película.
+		Se puede utilizar en la línea de tiempo principal o en líneas de tiempo de clips de película.
+		
+		Instrucciones:
+		1. Reemplace el número 5 del siguiente código por el número de fotograma hasta el que quiere que se mueva la cabeza lectora cuando se haga clic en la instancia del símbolo.
+		2.Los números de fotograma en EaselJS empiezan con 0 en vez de 1
+		*/
+		
+		
+		
+		
+		
+		/* Hacer clic para ir al fotograma y reproducir
+		Al hacer clic en la instancia del símbolo especificado, la cabeza lectora se mueve hasta el fotograma especificado en la línea de tiempo y prosigue la reproducción desde dicho fotograma.
+		Se puede utilizar en la línea de tiempo principal o en líneas de tiempo de clips de película.
+		
+		Instrucciones:
+		1. Reemplace el número 5 del siguiente código por el número de fotograma hasta el que quiere que se mueva la cabeza lectora cuando se haga clic en la instancia del símbolo.
+		2.Los números de fotograma en EaselJS empiezan con 0 en vez de 1
+		*/
+		
+		this.movieClip_4.addEventListener("click", fl_ClickToGoToAndPlayFromFrame_4.bind(this));
+		
+		function fl_ClickToGoToAndPlayFromFrame_4()
+		{
+			this.gotoAndPlay(1);
+		}
 		this.button_3.addEventListener("click", fl_ClickToGoToAndPlayFromFrame_3.bind(Ealbedo));
 		
-		function fl_ClickToGoToAndPlayFromFrame_3()
+		function fl_ClickToGoToAndPlayFromFrame_3() {
+			this.Ealbedo.gotoAndPlay(2);
+		}
+		this.INICIO.addEventListener("click", fl_ClickToGoToAndStopAtFrame_3.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_3()
 		{
-			Ealbedo.gotoAndPlay(2);
+			this.gotoAndStop(1);
 		}
 	}
 	this.frame_2 = function() {
@@ -416,21 +466,11 @@ if (reversed == null) { reversed = false; }
 		{
 			this.gotoAndPlay(4);
 		}
-		/* Hacer clic para ir al fotograma y detener
-		Al hacer clic en la instancia del símbolo especificado, la cabeza lectora se mueve hasta el fotograma especificado en la línea de tiempo y detiene la película.
-		Se puede utilizar en la línea de tiempo principal o en líneas de tiempo de clips de película.
+		this.INICIO.addEventListener("click", fl_ClickToGoToAndStopAtFrame_3.bind(this));
 		
-		Instrucciones:
-		1. Reemplace el número 5 del siguiente código por el número de fotograma hasta el que quiere que se mueva la cabeza lectora cuando se haga clic en la instancia del símbolo.
-		2.Los números de fotograma en EaselJS empiezan con 0 en vez de 1
-		*/
-		
-		
-		this.INICIO.addEventListener("click", fl_ClickToGoToAndplayAtFrame_3.bind(this));
-		
-		function fl_ClickToGoToAndplayAtFrame_3()
+		function fl_ClickToGoToAndStopAtFrame_3()
 		{
-			this.gotoAndplay(1);
+			this.gotoAndStop(1);
 		}
 	}
 	this.frame_3 = function() {
@@ -444,7 +484,7 @@ if (reversed == null) { reversed = false; }
 		
 		function fl_ClickToGoToAndPlayFromFrame_3()
 		{
-			Ealbedo.gotoAndPlay(1);
+			this.Ealbedo.gotoAndPlay(1);
 		}
 	}
 
@@ -452,10 +492,10 @@ if (reversed == null) { reversed = false; }
 	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1).call(this.frame_1).wait(1).call(this.frame_2).wait(1).call(this.frame_3).wait(2));
 
 	// TextoINTRO
-	this.instance = new lib.CachedBmp_16();
+	this.instance = new lib.CachedBmp_2();
 	this.instance.setTransform(29.35,68,0.5,0.5);
 
-	this.instance_1 = new lib.CachedBmp_15();
+	this.instance_1 = new lib.CachedBmp_1();
 	this.instance_1.setTransform(30.4,72.05,0.5,0.5);
 
 	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).to({state:[]},1).wait(4));
@@ -469,7 +509,7 @@ if (reversed == null) { reversed = false; }
 	this.instance_3.setTransform(434.5,408,1,1,0,0,0,80.5,92);
 	new cjs.ButtonHelper(this.instance_3, 0, 1, 1);
 
-	this.instance_4 = new lib.CachedBmp_17();
+	this.instance_4 = new lib.CachedBmp_3();
 	this.instance_4.setTransform(323.4,501.2,0.5,0.5);
 
 	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_4},{t:this.instance_3},{t:this.instance_2}]}).to({state:[]},1).wait(4));
@@ -495,7 +535,7 @@ if (reversed == null) { reversed = false; }
 	this.Ealbedo.setTransform(644.5,772.95,1,1,0,0,0,455.5,600);
 	this.Ealbedo._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.Ealbedo).wait(2).to({_off:false},0).wait(1).to({_off:true},1).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.Ealbedo).wait(1).to({_off:false},0).wait(2).to({_off:true},1).wait(1));
 
 	// botones
 	this.button_3 = new lib.posteralbedo();
@@ -506,22 +546,27 @@ if (reversed == null) { reversed = false; }
 
 	this.timeline.addTween(cjs.Tween.get(this.button_3).wait(1).to({_off:false},0).wait(2).to({_off:true},1).wait(1));
 
-	// FONDO2
-	this.instance_6 = new lib.FONDO2();
-	this.instance_6.setTransform(-83,0,1.1362,1.1362);
-
-	this.INICIO = new lib.HOME();
+	// INICIO
+	this.INICIO = new lib.Símbolo7();
 	this.INICIO.name = "INICIO";
-	this.INICIO.setTransform(50,48,1,1,0,0,0,26,26);
+	this.INICIO.setTransform(63,52);
+	this.INICIO._off = true;
 	new cjs.ButtonHelper(this.INICIO, 0, 1, 1);
 
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.instance_6}]},1).to({state:[{t:this.instance_6},{t:this.INICIO}]},1).to({state:[]},2).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.INICIO).wait(1).to({_off:false},0).wait(1).to({_off:true},1).wait(2));
+
+	// FONDO2
+	this.instance_6 = new lib.FONDO2();
+	this.instance_6.setTransform(-78,0,1.1362,1.1362);
+	this.instance_6._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_6).wait(1).to({_off:false},0).wait(1).to({x:-83},0).to({_off:true},2).wait(1));
 
 	// FONDOINICIO
-	this.instance_7 = new lib.CachedBmp_20();
+	this.instance_7 = new lib.CachedBmp_6();
 	this.instance_7.setTransform(896.15,10.3,0.5,0.5);
 
-	this.instance_8 = new lib.CachedBmp_19();
+	this.instance_8 = new lib.CachedBmp_5();
 	this.instance_8.setTransform(22,10.3,0.5,0.5);
 
 	this.instance_9 = new lib.Símbolo13("synched",0);
@@ -530,7 +575,7 @@ if (reversed == null) { reversed = false; }
 	this.instance_10 = new lib.GLOBALSTRIPESCHOOSEYOURFUTURE();
 	this.instance_10.setTransform(1,-8,0.9143,0.9239);
 
-	this.instance_11 = new lib.CachedBmp_18();
+	this.instance_11 = new lib.CachedBmp_4();
 	this.instance_11.setTransform(-0.5,-0.5,0.5,0.5);
 
 	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_11},{t:this.instance_10},{t:this.instance_9},{t:this.instance_8},{t:this.instance_7}]}).to({state:[]},1).wait(4));
@@ -538,7 +583,7 @@ if (reversed == null) { reversed = false; }
 	this._renderFirstFrame();
 
 }).prototype = p = new lib.AnMovieClip();
-p.nominalBounds = new cjs.Rectangle(0,0,1284,778.3);
+p.nominalBounds = new cjs.Rectangle(0,0,1285.5,778.3);
 // library properties:
 lib.properties = {
 	id: '7576601AE606A14F940B9647A899AF49',
@@ -548,9 +593,9 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/CachedBmp_18.png?1762182231627", id:"CachedBmp_18"},
-		{src:"images/Sesión de trabajo_atlas_1.png?1762182231610", id:"Sesión de trabajo_atlas_1"},
-		{src:"images/Sesión de trabajo_atlas_2.png?1762182231610", id:"Sesión de trabajo_atlas_2"}
+		{src:"images/CachedBmp_4.png?1762194941434", id:"CachedBmp_4"},
+		{src:"images/Sesión de trabajo_atlas_1.png?1762194941418", id:"Sesión de trabajo_atlas_1"},
+		{src:"images/Sesión de trabajo_atlas_2.png?1762194941418", id:"Sesión de trabajo_atlas_2"}
 	],
 	preloads: []
 };
